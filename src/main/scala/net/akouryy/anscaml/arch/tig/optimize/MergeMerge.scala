a31d1ca16d7d584f1ee076c1e815ac719395dc77
akouryy <akouryy7@gmail.com>
2019-12-14 01:46:07 +0900

    Allow MergeMerge to merge two Merges between which there is only one nice line

--- src/main/scala/net/akouryy/anscaml/arch/tig/optimize/MergeMerge.scala
+++ src/main/scala/net/akouryy/anscaml/arch/tig/optimize/MergeMerge.scala
@@ -3,9 +3,13 @@ package arch.tig
 package optimize
 
 import asm._
+import base._
 
 import scala.collection.mutable
 
+/**
+  * 0〜1行のブロックを挟んでMergeが続いているとき、それを合併する
+  */
 object MergeMerge {
   def apply(program: Program): Boolean = {
     var changed = false
@@ -13,17 +17,43 @@ object MergeMerge {
       for ((_, m0 @ Merge(_, ji0, inputs0, _, _)) <- f.body.jumps) { // 0が下、3が上
         val newInputs0 = inputs0.map(_.toPair).to(mutable.SortedMap)
         for {
-          MergeInput(bi1, toMerge0) <- inputs0
-          Block(_, Nil, ji2, _) <- Some(f.body.blocks(bi1))
-          Merge(_, _, inputs2, merged2, _) <- Some(f.body.jumps(ji2))
+          MergeInput(bi1, phiArg0) <- inputs0
+          Block(_, lines1, ji2, _) <- Some(f.body.blocks(bi1))
+          if lines1.sizeIs <= 1
+          lineOpt1 = lines1.headOption
+          Merge(_, _, inputs2, phiRes2, _) <- Some(f.body.jumps(ji2))
+          if lineOpt1.forall(line1 => line1.dest == phiArg0 && phiRes2 == XReg.DUMMY)
         } {
+          /*
+            1. line1がないとき
+              - Merge0とMerge2のφ関数(phi0とphi2)がどちらもあるとき、phi2の返り値がphi0の引数になっている
+                (そうでない場合はphi2は不要である)
+            2. line1があって、そのdestがphi0の引数に等しく、phi2がないとき
+              - phiNew0の引数newPhiArg0を新たに作る
+              - line1のdestを新たな変数newPhiArg0に変える
+              //// - line1のinstのうちphi2の返り値だった部分をphi2の引数に変える
+              - 元の2からφを継承する必要がない
+          */
           newInputs0 -= bi1
           f.body.blocks -= bi1
           f.body.jumps -= ji2
-          for (MergeInput(bi3, toMerge2) <- inputs2) {
-            f.body.blocks(bi3) = f.body.blocks(bi3).copy(output = ji0)
-            // outputID0に行くID
-            newInputs0(bi3) = if (merged2 == toMerge0) toMerge2 else toMerge0
+          for (MergeInput(bi3, phiArg2) <- inputs2) {
+            val b3 = f.body.blocks(bi3)
+
+            lineOpt1 match {
+              case None =>
+                f.body.blocks(bi3) = b3.copy(output = ji0)
+                // phiNew0の引数
+                newInputs0(bi3) = if (phiRes2 == phiArg0) phiArg2 else phiArg0
+              case Some(line1) if line1.dest == phiArg0 && phiRes2 == XReg.DUMMY =>
+                val newPhiArg0 = XVar.generate(line1.dest.idStr)
+                f.body.blocks(bi3) = b3.copy(
+                  output = ji0,
+                  lines = b3.lines :+ line1.copy(dest = newPhiArg0),
+                )
+                newInputs0(bi3) = newPhiArg0
+              case _ => ????(lineOpt1)
+            }
           }
           changed = true
         }
